eb122ba667d2dc9ebc0860be6cdd8459fb4a75eb
tulio.resende <tulio.resende@dtidigital.com.br>
2022-02-10 18:29:57 -0300

    fix class name of subjectController

--- Web_App/Controllers/UseCases/Topics/GetTopicsFromProject/TopicsController.cs
+++ Web_App/Controllers/UseCases/Topics/GetTopicsFromProject/TopicsController.cs
@@ -4,25 +4,22 @@ using System.Threading.Tasks;
 
 namespace Web_App.Controllers.UseCases.Topics.GetTopicsFromProject
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class TopicsController : ControllerBase
     {
-        [ApiController]
-        [Route("api/[controller]")]
-        public class SubjectsController : ControllerBase
+        private readonly IGetTopicsFromSubjectUseCase getTopicsFromSubject;
+
+        public TopicsController(IGetTopicsFromSubjectUseCase getTopicsFromSubject)
         {
-            private readonly IGetTopicsFromSubjectUseCase getTopicsFromSubject;
+            this.getTopicsFromSubject = getTopicsFromSubject;
+        }
 
-            public SubjectsController(IGetTopicsFromSubjectUseCase getTopicsFromSubject)
-            {
-                this.getTopicsFromSubject = getTopicsFromSubject;
-            }
-
-            [HttpGet("GetTopicsFromSubject/{subjectId}")]
-            public async Task<IActionResult> Get(int subjectId)
-            {
-                var data = await getTopicsFromSubject.GetTopicsFromSubject(subjectId);
-                return Ok(data);
-            }
+        [HttpGet("GetTopicsFromSubject/{subjectId}")]
+        public async Task<IActionResult> Get(int subjectId)
+        {
+            var data = await getTopicsFromSubject.GetTopicsFromSubject(subjectId);
+            return Ok(data);
         }
     }
 }

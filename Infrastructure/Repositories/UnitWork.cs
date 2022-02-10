5b86a4664dd7e2e0a135893ff148fc176d128f1f
tulio.resende <tulio.resende@dtidigital.com.br>
2022-02-10 19:07:45 -0300

    creation unit of work to save changes in database

---
+++ Infrastructure/Repositories/UnitWork.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Context;
+using Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class UnitWork : IUnitWork
+    {
+        private AppDbContext _context;
+
+        public UnitWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}

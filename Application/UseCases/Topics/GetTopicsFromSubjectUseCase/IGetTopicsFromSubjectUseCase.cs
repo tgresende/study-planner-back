4fdb7606bf5b8f9d8684dec0ffdef3600998909d
tulio.resende <tulio.resende@dtidigital.com.br>
2022-02-10 18:03:06 -0300

    add useCase to get topics from subject

---
+++ Application/UseCases/Topics/GetTopicsFromSubjectUseCase/IGetTopicsFromSubjectUseCase.cs
@@ -0,0 +1,14 @@
+﻿using Domain.ReponseModels.Topic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Topics.GetTopicsFromSubjectUseCase
+{
+    public interface IGetTopicsFromSubjectUseCase
+    {
+        public Task<List<GetTopicsFromSubjectResponseModel>> GetTopicsFromSubject(int subjectId);
+    }
+}

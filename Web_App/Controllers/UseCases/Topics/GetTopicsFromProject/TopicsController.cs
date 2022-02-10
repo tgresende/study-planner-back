43eb6d9f6e8c1fa38ab8cf63302eb5071640d1da
tulio.resende <tulio.resende@dtidigital.com.br>
2022-02-10 18:12:18 -0300

    add Topics Controller to get topics from subject use case

---
+++ Web_App/Controllers/UseCases/Topics/GetTopicsFromProject/TopicsController.cs
@@ -0,0 +1,28 @@
+﻿using Application.UseCases.Topics.GetTopicsFromSubjectUseCase;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Web_App.Controllers.UseCases.Topics.GetTopicsFromProject
+{
+    public class TopicsController : ControllerBase
+    {
+        [ApiController]
+        [Route("api/[controller]")]
+        public class SubjectsController : ControllerBase
+        {
+            private readonly IGetTopicsFromSubjectUseCase getTopicsFromSubject;
+
+            public SubjectsController(IGetTopicsFromSubjectUseCase getTopicsFromSubject)
+            {
+                this.getTopicsFromSubject = getTopicsFromSubject;
+            }
+
+            [HttpGet("GetTopicsFromSubject/{subjectId}")]
+            public async Task<IActionResult> Get(int subjectId)
+            {
+                var data = await getTopicsFromSubject.GetTopicsFromSubject(subjectId);
+                return Ok(data);
+            }
+        }
+    }
+}

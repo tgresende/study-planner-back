1721bff3f0a78afc66b740fca45284eb83f4ccd5
tulio.resende <tulio.resende@dtidigital.com.br>
2022-02-11 08:04:28 -0300

    add action types

---
+++ Domain/Types/TopicTask/ActionTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Types.TopicTask
+{
+    public class ActionTypes
+    {
+        public const string Reading = "Leitura";
+        public const string RevisionQuestions = "Questões de Revisão";
+        public const string Exercises = "Questões";
+
+        public List<string> GetActionsList()
+        {
+            return new List<string> { Reading, RevisionQuestions, Exercises };
+        }
+    }
+}

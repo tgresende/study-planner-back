c433fbc6218131b74764fe163c41c5a67e5e1a8b
tulio.resende <tulio.resende@dtidigital.com.br>
2022-02-12 18:23:06 -0300

    tentando fazer o join

---
+++ Application/UseCases/TopicTasks/GetActiveTopicTasksUseCase/GetActiveTopicTasksUseCase.cs
@@ -0,0 +1,61 @@
+﻿using Application.Services.Notifications;
+using Domain.Entities;
+using Infrastructure.Interfaces;
+using Infrastructure.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.TopicTasks.GetActiveTopicTasksUseCase
+{
+    public class GetActiveTopicTasksUseCase : IGetActiveTopicTasksUseCase
+    {
+        private readonly ITopicTaskRepository topicTaskRepository;
+        private readonly INotification notification;
+        private readonly ISubjectRepository subjectRepository;
+
+        public GetActiveTopicTasksUseCase(
+            ITopicTaskRepository topicTaskRepository,
+            INotification notification,
+            ISubjectRepository subjectRepository
+        )
+        {
+            this.topicTaskRepository = topicTaskRepository;
+            this.notification = notification;
+            this.subjectRepository = subjectRepository;
+        }
+
+        public async Task<List<GetActiveTopicTaskResponseModel>?> GetActiveTopicTasks(int subjectId)
+        {
+            Subject subject = await subjectRepository.GetSubject(subjectId);
+
+            if (subject == null)
+            {
+                notification.AddErrorMessage("Assunto não encontrado");
+                return null;
+            }
+
+            var task = await topicTaskRepository.GetActiveTopicTasks(subject);
+            List<GetActiveTopicTaskResponseModel> respondeModelList =
+                new List<GetActiveTopicTaskResponseModel>();
+            foreach (TopicTask topicTask in task)
+            {
+                respondeModelList.Add(ConvertTopicTaskToGetActiveTopicTaskResponseModel(topicTask));
+            };
+
+            return respondeModelList;
+        }
+
+        private GetActiveTopicTaskResponseModel ConvertTopicTaskToGetActiveTopicTaskResponseModel(TopicTask task)
+        {
+            return new GetActiveTopicTaskResponseModel
+            {
+                Action = task.Action,
+                ActionDescription = task.ActionDescription,
+                ActionSource = task.ActionSource,
+                Status = (int)task.Status,
+                TopicId = task.Topic.TopicId,
+                TopicTaskId = task.TopicTaskId
+            };
+        }
+    }
+}
--- Infrastructure/Repositories/TopicTaskRepository.cs
+++ Infrastructure/Repositories/TopicTaskRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Entities;
 using Infrastructure.Context;
 using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,14 @@ namespace Infrastructure.Repositories
         public async Task InsertNewTopicTask(TopicTask topicTask)
         {
             await context.TopicTasks.AddAsync(topicTask);
+        }
+
+        public async Task<List<TopicTask>> GetActiveTopicTasks(Subject subject)
+        {
+            Subject sub = await context.Subjects
+                .Include(sub => sub.Topics)
+                .ThenInclude(top => top.TopicTasks)
+                .FirstOrDefault(sub => sub.SubjectId == subject.SubjectId);
         }
     }
 }

c36fae883e15819206d3dcaad841906dabece755
tulio.resende <tulio.resende@dtidigital.com.br>
2022-02-13 08:36:19 -0300

    create annotations in subject entity and database

--- Infrastructure/Migrations/20220213113445_create_annotations_field_in_subject_table.Designer.cs
+++ Infrastructure/Migrations/20220213113445_create_annotations_field_in_subject_table.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220213113445_create_annotations_field_in_subject_table")]
+    partial class create_annotations_field_in_subject_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
